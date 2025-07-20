ed50198fae7b965749fb37aa87053b5c3ac76ffb
Artal44 <arthur.dandoy02@gmail.com>
2025-07-20 16:06:19 +0200

    nav_measure pid in state

--- balancing_robot/src/nav_measure.erl
+++ balancing_robot/src/nav_measure.erl
@@ -3,10 +3,10 @@
 
 -export([init/1, measure/1]).
 
-init([Role]) ->
+init([Pid, Role]) ->
     Name = list_to_atom("NAV_" ++ atom_to_list(Role)),
     io:format("[NAV] Starting ~p~n", [Role]),
-    {ok, #{seq => 1, role => Role}, #{
+    {ok, #{seq => 1, target => Pid, role => Role}, #{
         name => Name,
         iter => infinity,
         timeout => 5
@@ -15,7 +15,7 @@ init([Role]) ->
 measure(State) ->
     try
         [Gy, Ax, Az] = pmod_nav:read(acc, [out_y_g, out_x_xl, out_z_xl], #{g_unit => dps}),
-        Pid = persistent_term:get(pid_main),
+        Pid = maps:get(target, State),
         Pid ! {nav_data, [Gy, Ax, Az]},
         Seq = maps:get(seq, State),
         NewState = State#{seq => Seq + 1},
