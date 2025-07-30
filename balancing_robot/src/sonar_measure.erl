177413afb3ebe6b2355c57868cf41364ef610cae
Artal44 <arthur.dandoy02@gmail.com>
2025-07-30 11:46:31 +0200

    low pass filter

--- balancing_robot/src/sonar_measure.erl
+++ balancing_robot/src/sonar_measure.erl
@@ -6,8 +6,7 @@
 init([Role]) ->
     Name = list_to_atom("SONAR_" ++ atom_to_list(Role)),
     io:format("[SONAR] Starting ~p~n", [Role]),
-    Tnow = erlang:system_time(millisecond),
-    {ok, #{seq => 1, role => Role, last_distance => none, last_time => Tnow}, #{
+    {ok, #{seq => 1, role => Role, last_distance => none}, #{
         name => Name,
         iter => infinity,
         timeout => 50  %% => on attend uniquement les authorize
@@ -17,10 +16,6 @@ measure(State) ->
     Role = maps:get(role, State),
     receive
         {authorize, Sender} ->
-            Tnow = erlang:system_time(millisecond),
-            PrevT = maps:get(last_time, State),
-            Dt = Tnow - PrevT,
-
             RawD = measure_distance(),
             PrevD = maps:get(last_distance, State),
             FilteredD =
@@ -30,15 +25,14 @@ measure(State) ->
                     _ ->
                         % Filtering logic
                         Valid = RawD >= 15.0 andalso RawD =< 648.0,
-                        if Valid -> filter(RawD, PrevD, Dt);
+                        if Valid -> filter(RawD, PrevD);
                         true -> PrevD end
                 end,
 
             Seq = maps:get(seq, State),
             NewState = State#{
                 seq => Seq + 1,
-                last_distance => FilteredD,
-                last_time => Tnow
+                last_distance => FilteredD
             },
             io:format("[SONAR] New measure taken: ~p", [FilteredD]),
             send_to_main(Role, Sender, FilteredD, Seq),
@@ -55,13 +49,8 @@ round_to(Value, Precision) ->
     Factor = math:pow(10, Precision),
     round(Value * Factor) / Factor.
 
-filter(New, Prev, Dt) ->
-    Alpha = compute_alpha(Dt), % pondération dynamique selon Dt
-    round_to(low_pass_filter(New, Prev, Alpha), 2).
-
-compute_alpha(Dt) when Dt < 200 -> 0.7;
-compute_alpha(Dt) when Dt < 500 -> 0.5;
-compute_alpha(_) -> 0.3.
+filter(New, Prev) ->
+    round_to(low_pass_filter(New, Prev, 0.15), 2).
 
 low_pass_filter(RawD, LastD, Alpha) ->
     case LastD of
